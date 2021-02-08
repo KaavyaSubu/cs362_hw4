660fe7c93d7cb21aa6eb82d7556f87ad56e6f2ad
Kaavya Subramanian <subramka@flip3.engr.oregonstate.edu>
2021-02-07 21:54:11 -0800

    everything is wokring

---
+++ hw4_fxns.py
@@ -0,0 +1,48 @@
+ #Code for Question 1
+
+def calcCubeVol(side):
+
+
+    try:
+        side = float(side)
+        try:
+            if side <= 0: raise ValueError
+            else: return pow(side, 3)
+        except ValueError:
+            print("Not a valid input.")
+
+    except ValueError:
+        print("Not a float.")
+
+
+
+
+ # Code for Question 2
+def calcListAvg(l):
+    try:
+        if len(l) == 0: raise ZeroDivisionError
+
+        try:
+            check = True
+            for x in l:
+                if isinstance(x, float) or isinstance(x, int): continue
+                else: check = False
+
+
+            if check == False: raise ValueError
+
+            else:
+                avg = float(sum(l))/len(l)
+                return avg
+        except ValueError:
+            print("Not all items in the list are numbers.")
+
+
+    except ZeroDivisionError:
+        print("List is empty.")
+
+ # Code for Question 3
+
+def genFullName(first, last):
+
+    return str(first) + " " + str(last)
---
+++ test_hw4_fxns.py
@@ -0,0 +1,36 @@
+import unittest
+import hw4_fxns
+from hw4_fxns import calcCubeVol
+from hw4_fxns import calcListAvg
+from hw4_fxns import genFullName
+
+class TestCase(unittest.TestCase):
+
+        #Tests for Question 1
+        def test_q1test1(self):
+            self.assertEqual(calcCubeVol(4),64)
+        def test_q1test2(self):
+            self.assertRaises(ValueError,calcCubeVol("forty"))
+        def test_q1test3(self):
+            self.assertRaises(ValueError,calcCubeVol(-5))
+
+        #Tests for Question 2
+        def test_q2test1(self):
+            self.assertEqual(calcListAvg([3,3,1,4,5]),3.2)
+
+        def test_q2test2(self):
+            self.assertRaises(ZeroDivisionError,calcListAvg([]))
+        def test_q3test3(self):
+            self.assertRaises(ValueError,calcListAvg([1,2,"abc"]))
+
+        #Tests for Question 3
+        def test_q3test1(self):
+            self.assertEqual(genFullName("Kaavya","Subramanian"), "Kaavya Subramanian")
+        def test_q3test2(self):
+            self.assertEqual(genFullName("Wonder", "Woman"), "Wonder Woman")
+        def test_q3test3(self):
+            self.assertEqual(genFullName("Hermione", "Granger"), "Hermione Granger")
+
+
+if __name__ == '__main__':
+    unittest.main()
